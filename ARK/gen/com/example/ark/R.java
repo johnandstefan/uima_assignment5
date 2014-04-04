5809c99afb839b03fecc34aa09edd7cf530261da
sreichenstein <stefan.reichenstein@gmail.com>
2014-04-03 21:33:22 -0400

    Not sure what was updated. everything works
    Check if there is anything else that needs to be done/edited/fixed
    
    Remove from the spinner is a pain.

--- ARK/gen/com/example/ark/R.java
+++ ARK/gen/com/example/ark/R.java
@@ -26,12 +26,12 @@ public final class R {
     public static final class id {
         public static final int action_settings=0x7f08000a;
         public static final int add_message=0x7f080009;
-        public static final int button1=0x7f080007;
+        public static final int button1=0x7f080005;
         public static final int contact_name=0x7f080006;
         public static final int contact_number=0x7f080004;
         public static final int main_title=0x7f080000;
         public static final int randomize_recipient=0x7f080002;
-        public static final int randomize_text=0x7f080005;
+        public static final int randomize_text=0x7f080007;
         public static final int send=0x7f080001;
         public static final int spinner=0x7f080008;
         public static final int text_input=0x7f080003;
@@ -44,6 +44,7 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int add_message=0x7f050009;
         public static final int app_name=0x7f050000;
         public static final int contact_hint=0x7f050006;
         public static final int hello_world=0x7f050002;
